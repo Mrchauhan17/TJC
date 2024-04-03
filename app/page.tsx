bec8f2683a1b3bb31ee46936d6c90a855e48e8a7
Tinku <tinkusingh991056@gmail.com>
2024-04-03 16:34:19 +0530

    ui corrected

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,14 @@ export default function Home() {
         <div className="color"></div>
         <div className="color"></div>
         <div className="box">
-          <div className="square" style={{ "--i": 0 }}></div>
-          <div className="square" style={{ "--i": 1 }}></div>
-          <div className="square" style={{ "--i": 2 }}></div>
-          <div className="square" style={{ "--i": 3 }}></div>
-          <div className="square" style={{ "--i": 4 }}></div>
+          <div className="square" style={{ "--i": 0 } as any}></div>
+          <div className="square" style={{ "--i": 1 } as any}></div>
+          <div className="square" style={{ "--i": 2 } as any}></div>
+          <div className="square" style={{ "--i": 3 } as any}></div>
+          <div className="square" style={{ "--i": 4 } as any}></div>
           <div className="container">
+            {/* Content */}
+
             <div className="form">
               <h2>Login Form</h2>
               <form id="form"action = "api/user">
